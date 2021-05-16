8dd8b47a1c632a674c704da77db009037c5e0aea
Tato Berante <tato@berante.com.ar>
2021-05-16 20:52:22 -0300

    Added config.php for basep project.

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$appname = "HM2";
+$logo = "../v/img/logo.png";
+$db = "basep";
+$dbusr = "root";
+$dbkey = "Nurgle2020";
+$ftp = "ftp://200.110.219.83/";
+$charset = "utf8mb4_unicode_ci";
+?>
